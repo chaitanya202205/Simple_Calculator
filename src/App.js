522426e0884328acd4739f5100093bd3acb50c0a
chaitanya202205 <chaitanyasaikota05@gmail.com>
2024-07-01 15:59:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+import Calculator from './Calculator';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
